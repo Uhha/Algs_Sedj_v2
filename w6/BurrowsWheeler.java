86cc553e812a88b9aee6242143ed68a3b15080f8
Uhha <asvinarenka@gmail.com>
2014-12-22 17:13:12 -0500

    70+

--- w6/BurrowsWheeler.java
+++ w6/BurrowsWheeler.java
@@ -30,13 +30,42 @@ public class BurrowsWheeler {
     
     // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
     public static void decode(){
+    	int num = BinaryStdIn.readInt();
     	String s = BinaryStdIn.readString();
+    	//int num = 3;
+    	//String s = "ARD!RCAAAABB";
+    	StringBuffer sb = new StringBuffer();
+    	
+    	int[] next = new int[s.length()];
+    	int[] R = new int[256+1];
+    	for (int i = 0; i < s.length(); i++) {
+			R[s.charAt(i)+1]++;
+		}
+    	for (int i = 1; i < R.length; i++) {
+			R[i] += R[i-1];
+		}
+    	for (int i = 0; i < s.length(); i++) {
+			int charAt = s.charAt(i);
+    		if(charAt == num) num = i;
+    		next[i] = R[charAt];
+			R[charAt]++;
+		}
+    	int cnt = 0;
+    	while (cnt < s.length()){
+    		sb.append(s.charAt(num));
+    		num = next[num];
+    		cnt++;
+    	}
+    	BinaryStdOut.write(sb.reverse().toString());
+    	BinaryStdOut.flush();
     	
     }
 
     // if args[0] is '-', apply Burrows-Wheeler encoding
     // if args[0] is '+', apply Burrows-Wheeler decoding
     public static void main(String[] args){
-    	
+    	 if      (args[0].equals("-")) encode();
+         else if (args[0].equals("+")) decode();
+         else throw new IllegalArgumentException("Illegal command line argument");
     }
 }
