44f2cf3ba2dd25f53ff98349178da4273c048786
Uhha <asvinarenka@gmail.com>
2014-12-23 17:02:46 -0500

    ;]

--- w6/MoveToFront_97.java
+++ w6/MoveToFront_97.java
@@ -1,33 +1,26 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.LinkedList;
 
-
-
-public class MoveToFront {
+public class MoveToFront_97 {
     // apply move-to-front encoding, reading from standard input and writing to standard output
-    public static void encode() throws FileNotFoundException{
-    	System.setIn(new FileInputStream("w6/burrows/encodedSecretMessage.txt"));
-
-    	LinkedList<Integer> R = new LinkedList<>();
-    	for (int i = 0; i < 256; i++) {
-			R.add(i);
+    public static void encode(){
+    	int[] R = new int[256];
+    	for (int i = 0; i < R.length; i++) {
+			R[i] = i;
 		}
     	while (!BinaryStdIn.isEmpty()){
     		int ch = BinaryStdIn.readChar();
-    		for (int i = 0; i < R.size(); i++) {
-				if(R.get(i) == ch) {
-					BinaryStdOut.write(R.get(i), 8);
-					R.add(0, R.remove(i));}
+    		BinaryStdOut.write(R[ch], 8);
+    		int cnt = 0;
+    		for (int i = 0; i < R.length; i++) {
+				if(R[i] < R[ch]) {R[i]++; cnt++;}
+				if(R[ch] < cnt) break;
 			}
+    		R[ch] = 0;
     	}
     	BinaryStdOut.flush();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
-    public static void decode() throws FileNotFoundException{
-    	System.setIn(new FileInputStream("w6/burrows/encodedSecretMessage.txt"));
-
+    public static void decode(){
     	int[] R = new int[256];
     	for (int i = 0; i < R.length; i++) {
 			R[i] = i;
@@ -47,7 +40,7 @@ public class MoveToFront {
 
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args){
     	if      (args[0].equals("-")) encode();
         else if (args[0].equals("+")) decode();
         else throw new IllegalArgumentException("Illegal command line argument");
